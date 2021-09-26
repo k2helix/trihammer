3488be1154824aff5a5017f30b3b330c2d83c5c5
Trunks <49949593+k2helix@users.noreply.github.com>
2021-09-26 13:18:52 +0200

    deferReply, then editReply

--- src/interactionCommands/social/profile.js
+++ src/interactionCommands/social/profile.js
@@ -28,60 +28,57 @@ module.exports = {
 	cooldown: 3,
 	type: 5,
 	myPerms: [true, 'ATTACH_FILES'],
-	async execute(client, interaction) {
-		interaction.deferReply();
-		let user = interaction.options.getUser('user') || interaction.user;
-		if (user.bot)
-			if (interaction.replied || interaction.deferred) return interaction.editReply({ content: 'Bots do not have profile!', ephemeral: true });
-			else return interaction.reply({ content: 'Bots do not have profile!', ephemeral: true });
+	execute(client, interaction) {
+		interaction.deferReply().then(async () => {
+			let user = interaction.options.getUser('user') || interaction.user;
+			if (user.bot) return interaction.editReply({ content: 'Bots do not have profile!', ephemeral: false });
 
-		let global = await ModelUsers.findOne({ id: user.id }).lean();
-		if (!global)
-			if (interaction.replied || interaction.deferred) return interaction.editReply({ content: '404 User not found (send some messages and try again)', ephemeral: true });
-			else return interaction.reply({ content: '404 User not found (send some messages and try again)', ephemeral: true });
+			let global = await ModelUsers.findOne({ id: user.id }).lean();
+			if (!global) return interaction.editReply({ content: '404 User not found (send some messages and try again)', ephemeral: false });
 
-		const xp = global.globalxp;
-		const canvas = createCanvas(442, 330);
-		var context = canvas.getContext('2d');
-		const ctx = canvas.getContext('2d');
+			const xp = global.globalxp;
+			const canvas = createCanvas(442, 330);
+			var context = canvas.getContext('2d');
+			const ctx = canvas.getContext('2d');
 
-		const background = await loadImage(global.pimage);
-		ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-		const fondo = await loadImage('https://cdn.discordapp.com/attachments/487962590887149603/716060007577419876/capas.png');
-		ctx.globalAlpha = 0.75;
-		ctx.drawImage(fondo, 0, 0, canvas.width, canvas.height);
+			const background = await loadImage(global.pimage);
+			ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+			const fondo = await loadImage('https://cdn.discordapp.com/attachments/487962590887149603/716060007577419876/capas.png');
+			ctx.globalAlpha = 0.75;
+			ctx.drawImage(fondo, 0, 0, canvas.width, canvas.height);
 
-		ctx.strokeStyle = '#74037b';
-		ctx.strokeRect(0, 0, canvas.width, canvas.height);
+			ctx.strokeStyle = '#74037b';
+			ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
-		ctx.font = user.tag.length < 12 ? '30px sans-serif' : '20px sans-serif';
-		ctx.fillStyle = '#0e0d0e';
-		ctx.fillText(user.tag, 130, 90);
+			ctx.font = user.tag.length < 12 ? '30px sans-serif' : '20px sans-serif';
+			ctx.fillStyle = '#0e0d0e';
+			ctx.fillText(user.tag, 130, 90);
 
-		ctx.font = '17px sans-serif';
-		ctx.fillStyle = '#0e0d0e';
-		ctx.fillText(global.pdesc, 135, 120);
+			ctx.font = '17px sans-serif';
+			ctx.fillStyle = '#0e0d0e';
+			ctx.fillText(global.pdesc, 135, 120);
 
-		ctx.font = '24px sans-serif';
-		ctx.fillStyle = '#0e0d0e';
-		ctx.fillText('|\n|\n|\n|\n|', 130, 180);
+			ctx.font = '24px sans-serif';
+			ctx.fillStyle = '#0e0d0e';
+			ctx.fillText('|\n|\n|\n|\n|', 130, 180);
 
-		ctx.font = '24px sans-serif';
-		ctx.fillStyle = '#0e0d0e';
-		ctx.fillText(`Total XP\n${xp}`, 32, 180);
+			ctx.font = '24px sans-serif';
+			ctx.fillStyle = '#0e0d0e';
+			ctx.fillText(`Total XP\n${xp}`, 32, 180);
 
-		ctx.font = '20px sans-serif';
-		ctx.fillStyle = '#ff0007';
-		ctx.fillText(`+${global.rep}rep`, 30, 295);
+			ctx.font = '20px sans-serif';
+			ctx.fillStyle = '#ff0007';
+			ctx.fillText(`+${global.rep}rep`, 30, 295);
 
-		context.fillStyle = '#0e0d0e';
-		context.font = '22px sans-serif';
-		wrapText(canvas.getContext('2d'), global.ptext, 140, 170, 275, 20);
+			context.fillStyle = '#0e0d0e';
+			context.font = '22px sans-serif';
+			wrapText(canvas.getContext('2d'), global.ptext, 140, 170, 275, 20);
 
-		const avatar = await loadImage(user.displayAvatarURL({ format: 'png' }));
-		ctx.drawImage(avatar, 27, 52, 100, 100);
+			const avatar = await loadImage(user.displayAvatarURL({ format: 'png' }));
+			ctx.drawImage(avatar, 27, 52, 100, 100);
 
-		const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'profile-image.png');
-		interaction.editReply({ files: [attachment] });
+			const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'profile-image.png');
+			interaction.editReply({ files: [attachment] });
+		});
 	}
 };
