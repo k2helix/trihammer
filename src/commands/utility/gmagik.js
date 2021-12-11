d8e5647e9a51b4c4d15df05f64d4cc8001ea641b
Trunks <49949593+k2helix@users.noreply.github.com>
2021-12-11 20:28:31 +0100

    open source

--- src/commands/utility/gmagik.js
+++ src/commands/utility/gmagik.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable require-await */
 /* eslint-disable no-unreachable */
 const { MessageAttachment } = require('discord.js');
 const request = require('node-superfetch');
@@ -13,32 +15,32 @@ module.exports = {
 	myPerms: [true, 'ATTACH_FILES'],
 	async execute(client, message, args) {
 		return message.channel.send('Command disabled');
-		const serverConfig = await ModelServer.findOne({ server: message.guild.id }).lean();
-		let langcode = serverConfig.lang;
-		let { util } = require(`../../utils/lang/${langcode}.js`);
-		let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
-		let image = user.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 });
-		let attachments = [...message.attachments.values()];
-		if (attachments[0]) image = attachments[0].url;
-		if (user.id === message.member.id && args[0] && args[0].startsWith('http')) image = args[0];
+		// const serverConfig = await ModelServer.findOne({ server: message.guild.id }).lean();
+		// let langcode = serverConfig.lang;
+		// let { util } = require(`../../utils/lang/${langcode}.js`);
+		// let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+		// let image = user.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 });
+		// let attachments = [...message.attachments.values()];
+		// if (attachments[0]) image = attachments[0].url;
+		// if (user.id === message.member.id && args[0] && args[0].startsWith('http')) image = args[0];
 
-		let msg = await message.channel.send(util.loading);
-		let result = await request.post('https://fapi.wrmsr.io/magikscript', {
-			headers: {
-				'Content-Type': 'application/json',
-				authorization: 'Bearer ' + process.env.FAPI_API_TOKEN
-			},
-			body: JSON.stringify({
-				images: [image],
-				args: {
-					text: 'magik',
-					gif: image.toLowerCase().includes('.gif') ? true : false
-				}
-			})
-		});
+		// let msg = await message.channel.send(util.loading);
+		// let result = await request.post('https://fapi.wrmsr.io/magikscript', {
+		// 	headers: {
+		// 		'Content-Type': 'application/json',
+		// 		authorization: 'Bearer ' + process.env.FAPI_API_TOKEN
+		// 	},
+		// 	body: JSON.stringify({
+		// 		images: [image],
+		// 		args: {
+		// 			text: 'magik',
+		// 			gif: image.toLowerCase().includes('.gif') ? true : false
+		// 		}
+		// 	})
+		// });
 
-		const attachment = new MessageAttachment(result.raw, 'magik.gif');
-		message.channel.send({ files: [attachment] });
-		msg.delete();
+		// const attachment = new MessageAttachment(result.raw, 'magik.gif');
+		// message.channel.send({ files: [attachment] });
+		// msg.delete();
 	}
 };
