87a2b2e274ec2b30083eae70854b9fb39ba3bde6
k2helix <49949593+k2helix@users.noreply.github.com>
2022-04-21 15:17:57 +0200

    rep and top interactions

--- src/interactionCommands/social/top.ts
+++ src/interactionCommands/social/top.ts
@@ -1,37 +1,35 @@
-const { ModelUsers, ModelRank } = require('../../lib/utils/models');
-module.exports = {
+import { CommandInteraction } from 'discord.js';
+import Command from '../../lib/structures/Command';
+import { ModelRank, ModelUsers, Rank, Users } from '../../lib/utils/models';
+export default new Command({
 	name: 'top',
 	description: 'Get the XP ranking of the server',
-	ESdesc: 'Obtén el top de XP del servidor',
-	usage: 'top [global]',
-	example: 'top\ntop global',
 	cooldown: 3,
-	aliases: ['leaderboard', 'ranking'],
-	type: 5,
+	category: 'social',
 	async execute(client, interaction) {
 		interaction.deferReply();
-		if (interaction.options.getBoolean('global')) {
-			let top = await ModelUsers.find({ globalxp: { $gte: 250000 } }).lean();
+		if ((interaction as CommandInteraction).options.getBoolean('global')) {
+			let top: Users[] = await ModelUsers.find({ globalxp: { $gte: 300000 } }).lean();
 			top.sort((a, b) => {
 				return b.globalxp - a.globalxp;
 			});
 			let usuarios = [];
-			for (var x in top.slice(0, 10)) {
+			for (let x in top.slice(0, 10)) {
 				let user = await client.users.fetch(top[x].id);
 				usuarios.push(`${parseFloat(x) + 1} - ${user.tag}:\nGlobal XP ${top[x].globalxp}`);
 			}
 
 			interaction.editReply(`** - 🏆 Global Rank -  **\n\`\`\`cs\n${usuarios.join('\n')}\`\`\``);
 		} else {
-			let top = await ModelRank.find({ server: interaction.guildId }).lean();
+			let top: Rank[] = await ModelRank.find({ server: interaction.guildId }).lean();
 			top.sort((a, b) => {
 				return b.nivel - a.nivel || b.xp - a.xp;
 			});
-			let posicion = (element) => element.id === interaction.user.id && element.server === interaction.guildId;
+			let posicion = (element: Rank) => element.id === interaction.user.id && element.server === interaction.guildId;
 			let usuarios = [];
 			// eslint-disable-next-line no-redeclare
-			for (var x in top.slice(0, 10)) {
-				let user = interaction.guild.members.cache.has(top[x].id) ? interaction.guild.members.cache.get(top[x].id).user.tag : 'Uncached user (' + top[x].id + ')';
+			for (let x in top.slice(0, 10)) {
+				let user = interaction.guild!.members.cache.has(top[x].id) ? interaction.guild!.members.cache.get(top[x].id)!.user.tag : 'Uncached user (' + top[x].id + ')';
 				usuarios.push(`${parseFloat(x) + 1} - ${user}:\nLevel ${top[x].nivel} ~ XP ${top[x].xp}`);
 			}
 			let lugar = top
@@ -46,4 +44,4 @@ module.exports = {
 			);
 		}
 	}
-};
+});
